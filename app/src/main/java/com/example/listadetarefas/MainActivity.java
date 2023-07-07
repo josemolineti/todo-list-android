009ebbfdaa3b45cf84fcc7e6b14864d6c7bbf400
joseem <molinetijose@gmail.com>
2023-07-07 17:25:11 -0300

    # Lista de Tarefas - Android # Commit 04 - Pref 07/07/23
    -- Finalizado parte dos cards
    -- implementada a funcionalidade de remover tast (prototipo)

--- app/src/main/java/com/example/listadetarefas/MainActivity.java
+++ app/src/main/java/com/example/listadetarefas/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.listadetarefas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        ImageView botaoAdd = findViewById(R.id.floatingActionButton);
+
+        botaoAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout linearLayout = findViewById(R.id.container_fundo);
+                View remove = findViewById(R.id.task1);
+                linearLayout.removeView(remove);
+                Toast.makeText(MainActivity.this, "Aaaaaaaaaaaaaa", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+
 
 
     }
